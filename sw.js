eacb7be6ea5776262566411c0ee96df99309f63b
Bruno <brunohsantos00@gmail.com>
2026-05-29 01:28:55 -0300

    fix: critical - urgentCount undefined crashed all widgets + bump SW v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bruno-tasks-v4';
+const CACHE_NAME = 'bruno-tasks-v5';
 const ASSETS = ['./index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
